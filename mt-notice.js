0fb43539812d8601d847911c2fc6997c55e8a99f
WlikesJim <jimwlikes@hotmail.com>
2023-05-21 03:45:27 +0800

    update

--- mt-notice.js
+++
@@ -1,5 +0,0 @@
-const popup = document.querySelector('.popup');
-const closeBtn = document.querySelector('.close-btn');
-function openPopup() { popup.style.display = 'flex'; }
-function closePopup() { popup.style.display = 'none'; }
-closeBtn.addEventListener('click', closePopup);
